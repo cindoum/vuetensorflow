efeb0c28f996b1432e76b08db8f6b61867243d81
Doum <dominic.lapointe@exova.com>
2018-10-31 14:40:39 -0400

    init

---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+const routerBase = process.env.USE_SUBFOLDER === 'true' ? {
+  router: {
+    base: '/vuenuxtensorflow/'
+  }
+} : {}
+
+module.exports = {
+  ...routerBase,
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'vuenuxtensorflow',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Tensorflow js simple project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    vendor: ['@tensorflow/tfjs'],
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
