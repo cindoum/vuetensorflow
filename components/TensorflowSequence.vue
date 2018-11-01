75cb57ca5212bba04005695333211394a0ee3115
Dominic Lapointe <dom.dlapointe@gmail.com>
2018-10-31 21:47:06 -0400

    mobilenet

--- components/TensorflowSequence.vue
+++ components/TensorflowSequence.vue
@@ -1,28 +1,87 @@
 <template>
   <div>
     <div class="train-controls">
-      <button class="button-add-example button--green" v-on:click="predict()">Predict</button>
+      <button class="button-add-example button--green" v-on:click="start()">Predict mobilenet</button>
+      <img id="initial_pic" src="~assets/plane.jpg" width="224" height="224" />
+      <pre>{{result}}</pre>
     </div>
   </div>
 </template>
 
 <script>
 import * as tf from "@tensorflow/tfjs";
+import {IMAGENET_CLASSES} from './kls.js';
+
 
 export default {
   data() {
     return {
-      mobilenet: null
+      mobilenet: null,
+      result: null
     };
   },
   methods: {
-    async predict() {
+    async start() {
       this.mobilenet = await tf.loadModel(
         "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json"
       );
+
+      this.mobilenet.predict(tf.zeros([1, 224, 224, 3])).dispose();
+
+      const pic = document.getElementById('initial_pic');
+
+      if (pic) {
+        this.predict(pic)
+      }
+    },
+    async predict(imgElement) {
+      const logits = tf.tidy(() => {
+          // tf.fromPixels() returns a Tensor from an image element.
+          const img = tf.fromPixels(imgElement).toFloat();
+
+          const offset = tf.scalar(127.5);
+          // Normalize the image from [0, 255] to [-1, 1].
+          const normalized = img.sub(offset).div(offset);
+
+          // Reshape to a single-element batch so we can pass it to predict.
+          const batched = normalized.reshape([1, 224, 224, 3]);
+
+          // Make a prediction through mobilenet.
+          return this.mobilenet.predict(batched);
+        });
+
+       const classes = await this.getTopKClasses(logits, 10);
+       this.result = classes;
+      },
+      async getTopKClasses(logits, topK) {
+        const values = await logits.data();
+
+        const valuesAndIndices = [];
+        for (let i = 0; i < values.length; i++) {
+          valuesAndIndices.push({value: values[i], index: i});
+        }
+        valuesAndIndices.sort((a, b) => {
+          return b.value - a.value;
+        });
+        const topkValues = new Float32Array(topK);
+        const topkIndices = new Int32Array(topK);
+        for (let i = 0; i < topK; i++) {
+          topkValues[i] = valuesAndIndices[i].value;
+          topkIndices[i] = valuesAndIndices[i].index;
+        }
+
+        const topClassesAndProbs = [];
+        for (let i = 0; i < topkIndices.length; i++) {
+          topClassesAndProbs.push({
+            className: IMAGENET_CLASSES[topkIndices[i]],
+            probability: topkValues[i]
+          })
+        }
+        return topClassesAndProbs;
+      }
+
     }
   }
-};
 </script>
 
 <style>
